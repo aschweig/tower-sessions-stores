e5172155dc1a37d74434bdc5323cb863fd32ed57
Aaron Schweiger <aaron.schweiger@gmail.com>
2024-09-07 17:20:40 -0400

    other approach

--- sqlx-store/src/lib.rs
+++ sqlx-store/src/lib.rs
@@ -1,26 +1,27 @@
 pub use sqlx;
 use tower_sessions_core::session_store;
 
-#[cfg(feature = "mysql")]
-#[cfg_attr(docsrs, doc(cfg(feature = "mysql")))]
+#[cfg(any(feature = "mysql",feature = "mysql-chrono"))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "mysql", feature = "mysql-chrono"))))]
 pub use self::mysql_store::MySqlStore;
-#[cfg(feature = "postgres")]
-#[cfg_attr(docsrs, doc(cfg(feature = "postgres")))]
+#[cfg(any(feature = "postgres",feature = "postgres-chrono"))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "postgres", feature = "postgres-chrono"))))]
 pub use self::postgres_store::PostgresStore;
-#[cfg(feature = "sqlite")]
-#[cfg_attr(docsrs, doc(cfg(feature = "sqlite")))]
+#[cfg(any(feature = "sqlite",feature = "sqlite-chrono"))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "sqlite", feature = "sqlite-chrono"))))]
 pub use self::sqlite_store::SqliteStore;
 
-#[cfg(feature = "sqlite")]
-#[cfg_attr(docsrs, doc(cfg(feature = "sqlite")))]
+#[cfg(any(feature = "sqlite", feature = "sqlite-chrono"))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "sqlite", feature = "sqlite-chrono"))))]
 mod sqlite_store;
 
 #[cfg(feature = "postgres")]
-#[cfg_attr(docsrs, doc(cfg(feature = "postgres")))]
+#[cfg(any(feature = "postgres",feature = "postgres-chrono"))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "postgres", feature = "postgres-chrono"))))]
 mod postgres_store;
 
-#[cfg(feature = "mysql")]
-#[cfg_attr(docsrs, doc(cfg(feature = "mysql")))]
+#[cfg(any(feature = "mysql",feature = "mysql-chrono"))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "mysql", feature = "mysql-chrono"))))]
 mod mysql_store;
 
 /// An error type for SQLx stores.
@@ -47,4 +48,27 @@ impl From<SqlxStoreError> for session_store::Error {
             SqlxStoreError::Encode(inner) => session_store::Error::Encode(inner.to_string()),
         }
     }
+}
+
+
+#[cfg(any(feature = "mysql", feature = "postgres", feature = "sqlite"))]
+pub fn current_time() -> time::OffsetDateTime {
+    time::OffsetDateTime::now_utc()
+}
+
+#[cfg(any(feature = "mysql-chrono", feature = "postgres-chrono", feature = "sqlite-chrono"))]
+pub fn current_time() -> chrono::DateTime<chrono::Utc> {
+    chrono::Utc::now()
+}
+
+#[cfg(any(feature = "mysql", feature = "postgres", feature = "sqlite"))]
+pub fn convert_expiry_date(expiry_date: time::OffsetDateTime) -> time::OffsetDateTime {
+    expiry_date
+}
+
+#[cfg(any(feature = "mysql-chrono", feature = "postgres-chrono", feature = "sqlite-chrono"))]
+pub fn convert_expiry_date(expiry_date: time::OffsetDateTime) -> chrono::DateTime<chrono::Utc> {
+    // if we can't convert the expiry date to a chrono type, return the current time i.e. effectively assume our session has expired
+    chrono::DateTime::from_timestamp(expiry_date.unix_timestamp(), expiry_date.nanosecond())
+        .unwrap_or(chrono::Utc::now())
 }
